52631cae77e3dbbf9f32c5c7288ce4e1566f5d00
Fabian Seitz <github@fabiseitz.de>
2026-08-04 11:02:42 +0200

    mypy linter fixes

--- custom_components/edeka/config_flow.py
+++ custom_components/edeka/config_flow.py
@@ -22,7 +22,7 @@ from .const import (
 _LOGGER = logging.getLogger(__name__)
 
 
-class EdekaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
+class EdekaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for EDEKA Offers."""
 
     VERSION = 1
